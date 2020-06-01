5da1da268318a36250a2d4a3771f360a088f997b
fireneve <firenive@gmail.com>
2020-06-01 17:03:20 +0300

    TheoryHashMap. changing Equals_HashCode

---
+++ src/JavaCollections/HashMap/TheoryHashMap.java
@@ -0,0 +1,6 @@
+package JavaCollections.HashMap;
+
+public class TheoryHashMap {
+    /* HashMap в своейо основе ипользует для хранения массив связаных списков. То есть в каждом элементе масива хранится
+    * связанный список. */
+}
